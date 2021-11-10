4a12a476d219583b87130150a7dc11da161cbe3a
심의석 <simsuli9@naver.com>
2021-11-10 21:18:53 +0900

    FEAT : #25 Implement OAuth Login

---
+++ src/main/java/com/jambit/project/domain/entity/Role.java
@@ -0,0 +1,14 @@
+package com.jambit.project.domain.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum Role {
+    GUEST("ROLE_GUEST", "손님"),
+    USER("ROLE_USER", "일반 사용자");
+
+    private final String key;
+    private final String title;
+}
--- src/main/java/com/jambit/project/dto/BoardDto.java
+++ src/main/java/com/jambit/project/dto/BoardDto.java
@@ -1,13 +1,17 @@
 package com.jambit.project.dto;
 
 import com.jambit.project.domain.entity.Board;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BoardDto {
 
     private Long id;
