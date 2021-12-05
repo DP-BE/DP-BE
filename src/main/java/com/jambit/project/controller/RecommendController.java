7a0617a2e9be7e370a3873000b14c8e919b81d08
최규식 <gyuman@choegyusig-ui-MacBookAir.local>
2021-12-05 19:30:31 +0900

    comment added

--- src/main/java/com/jambit/project/controller/RecommendController.java
+++ src/main/java/com/jambit/project/controller/RecommendController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 public class RecommendController {
     private final RecommendService recommendService;
 
+    // 구인글 좋아요 수
     @GetMapping("/count/post/{post_id}")
     public ResponseEntity<Integer> countPostRecommend(@PathVariable("post_id") Long postId) {
         Integer totalRecommend = recommendService.countRecommendByType(postId, TargetType.POST);
@@ -27,6 +28,7 @@ public class RecommendController {
         }
     }
 
+    // 프로젝트의 좋아요 수
     @GetMapping("/count/project/{project_id}")
     public ResponseEntity<Integer> countProjectRecommend(@PathVariable("project_id") Long projectId) {
         Integer totalRecommend = recommendService.countRecommendByType(projectId, TargetType.PROJECT);
@@ -38,6 +40,7 @@ public class RecommendController {
         }
     }
 
+    // 댓글의 좋아요
     @GetMapping("/count/reply/{reply_id}")
     public ResponseEntity<Integer> countReplyRecommend(@PathVariable("reply_id") Long replyId) {
         Integer totalRecommend = recommendService.countRecommendByType(replyId, TargetType.REPLY);
@@ -49,24 +52,28 @@ public class RecommendController {
         }
     }
 
+    // 구인글에 내가 좋아요 여부
     @GetMapping("/post/{post_id}/{nickname}")
     public ResponseEntity<Boolean> getPostRecommend(@PathVariable("post_id") Long postId, @PathVariable("nickname") String nickname) {
         Boolean isRecommend = recommendService.getRecommendByTypeAndUser(postId, TargetType.POST, nickname);
         return new ResponseEntity<>(isRecommend, HttpStatus.OK);
     }
 
+    // 프로젝트에 내가 좋아요 여부
     @GetMapping("/project/{project_id}/{nickname}")
     public ResponseEntity<Boolean> getProjectRecommend(@PathVariable("project_id") Long projectId, @PathVariable("nickname") String nickname) {
         Boolean isRecommend = recommendService.getRecommendByTypeAndUser(projectId, TargetType.PROJECT, nickname);
         return new ResponseEntity<>(isRecommend, HttpStatus.OK);
     }
 
+    // 댓글에 좋아요 여부
     @GetMapping("/reply/{reply_id}/{nickname}")
     public ResponseEntity<Boolean> getReplyRecommend(@PathVariable("reply_id") Long replyId, @PathVariable("nickname") String nickname) {
         Boolean isRecommend = recommendService.getRecommendByTypeAndUser(replyId, TargetType.REPLY, nickname);
         return new ResponseEntity<>(isRecommend, HttpStatus.OK);
     }
 
+    // 좋아요 등록
     @PostMapping("")
     public ResponseEntity<Long> registerRecommend(@RequestBody RecommendDto recommendDto) {
         Long recommendId = recommendService.create(recommendDto);
@@ -78,6 +85,7 @@ public class RecommendController {
         }
     }
 
+    // 좋아요 취소
     @DeleteMapping("/{recommend_id}")
     public ResponseEntity<Boolean> deleteRecommend(@PathVariable("recommend_id") Long recommendId) {
         Boolean isDeleted = recommendService.delete(recommendId);
